fe6dbc35e8aed1ca4f71370b078929fe4add2e09
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-05-07 15:54:03 +0100

    Today details displayed The Today weather elements are displayed. Did few modofications to the backend code around AJAX calls.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,65 @@
+function initVariables() {
+  weeklyWeather = [];
+  $("#today").empty();
+}
+
+function displayToday() {
+  var today = $("#today");
+  today.append(weeklyWeather[0].date);
+  icon = $("<img>");
+  icon.attr("src", "http://openweathermap.org/img/w/" + weeklyWeather[0].weatherIcon + ".png");
+  today.append(icon);
+  var temperature = $("<p>");
+  temperature.addClass("today-temp");
+  temperature.text("Temp: " + weeklyWeather[0].temp + " ");
+  temperature.append("<span>&#8451;</span>");
+
+  var wind = $("<p>");
+  wind.addClass("today-wind");
+  wind.text("Wind: " + weeklyWeather[0].wind + " km/h");
+
+  var humidity = $("<p>");
+  humidity.addClass("today-humidity");
+  humidity.text("Humidity: " + weeklyWeather[0].humidity + "%");
+
+
+  today.append(temperature);
+  today.append(wind);
+  today.append(humidity);
+
+};
+
+
 // CLICK HANDLERS
 // ==========================================================
 $("#search-button").on("click", function(event) {
   event.preventDefault();
 
+  initVariables();
+
   //see the structure in globalvariables
   cityWithCoordinates.city = $("#search-input").val().trim();
   
-  //call this back end function
-  callGeoCodingAPI(cityWithCoordinates.city);
+
+  $.when(
+    callGeoCodingAPI(cityWithCoordinates.city)
+    )
+    .done(function(geoCodingResponse){
+      parseGeoCodingResponse(geoCodingResponse);
+      $.when(
+        callForecastAPI()
+        )
+        .done(function(forecastResponse) {
+          parseForecastResponse(forecastResponse);
+          displayToday();
+      })
+    })
+    .then(function(){
+      console.log("then then theeeeeeen");
+    })
+    .fail(function(f1Val, f2Val){
+      console.log('fail!    f1, f2: ' + JSON.stringify([f1Val, f2Val]));
+    });
+
 });
 
